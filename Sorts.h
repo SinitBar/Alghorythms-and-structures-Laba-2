1a8e784818a329a950c4053ee90b36711c6ee17e
SinitBar <varka.sinitsckaya@yandex.ru>
2020-10-30 23:51:54 +0300

    Update Sorts.h

--- Sorts.h
+++ Sorts.h
@@ -10,7 +10,7 @@ class Timer
 private:
 	using clock_time = std::chrono::high_resolution_clock;
 	using second_time = std::chrono::duration<double, std::ratio<1>>;
-	std::chrono::time_point<clock_time> m_beg;
+	std::chrono::time_point<clock_time> begining;
 public:
 	Timer();
 	void reset();
@@ -18,16 +18,16 @@ public:
 	~Timer();
 };
 
-int* RandomMas(int size);
+int* RandomArray(int size);
 
-template <typename mas_elements_type >
-bool IsSorted(const mas_elements_type* mas, int size)
+template <typename array_elements_type >
+bool IsSorted(const array_elements_type* array, int size)
 {
 	if (size <= 1)
 		return true;
 	for (int i = 0; i < size - 1; i++)
 	{
-		if (mas[i] > mas[i + 1])
+		if (array[i] > array[i + 1])
 			return false;
 	}
 	return true;
@@ -36,6 +36,6 @@ bool IsSorted(const mas_elements_type* mas, int size)
 int BinarySearch(int*, int, int);
 void InsertionSort(int*, int);
 void BogoSort(int*, int);
-unsigned char* RandomCharMas(int);
+unsigned char* RandomCharArray(int);
 void CountingSort(unsigned char*, int);
 void QuickSort(int*, int);
